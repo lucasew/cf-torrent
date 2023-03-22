2077f1ced0c4514124e36850574aa5637c97213f
lucasew <lucas59356@gmail.com>
2023-03-21 22:04:43 -0300

    rankLinks: adjustments
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/lib/rankLinks.ts
+++ src/lib/rankLinks.ts
@@ -3,6 +3,7 @@ import { REGEX_IGNORED_DOMAINS } from "./constants";
 export function rankLinks(links: string[]): string[] {
   return links
     .filter((link) => !REGEX_IGNORED_DOMAINS.test(link)) // ignore unwanted words/domains
-    .sort((v) => v.match("torrent") ? -1 : 1 )           // priorize links with torrent in their name
+    .sort((x) => x.length)                               // priorize longer links, those are more likely to be posts instead of homepages
     .sort((v) => v.match("free") ? 1 : -1 )              // depriorize links with free in their name
+    .sort((v) => v.match("torrent") ? -1 : 1 )           // priorize links with torrent in their name
 }
