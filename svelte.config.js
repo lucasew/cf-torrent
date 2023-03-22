c2ddd2f74d4e7058c9e8b02a828bf49e1075ae24
lucasew <lucas59356@gmail.com>
2023-03-22 16:20:10 -0300

    multi adapter
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
+import nodeAdapter from '@sveltejs/adapter-node';
+import workersAdapter from '@sveltejs/adapter-cloudflare-workers';
+import multiAdapter from '@macfja/svelte-multi-adapter';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +15,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: multiAdapter([
+			nodeAdapter(),
+			autoAdapter(),
+			workersAdapter()
+		])
 	}
 };
 
